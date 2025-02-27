b1a55ee82ed0b9ec76567810ad83923791416d92
Duja Abdurhman <134400247+DujaAbdulrhman@users.noreply.github.com>
2025-02-27 12:15:52 +0300

    Update Shape.java

--- src/Shape.java
+++ src/Shape.java
@@ -2,48 +2,10 @@ public abstract class Shape {
     int x;
     int y;
 
-    public Shape(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+     abstract void calculateCircumference();
+
+     abstract void calculateArea();
 
 
-    public Shape(double radius) {
-    }
 
-    public Shape() {
-
-    }
-
-    public int getX() {
-        return x;
-    }
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public double calculateArea(){
-        calculateArea();
-        return 0;
-    }
-
-    public double calculateCircumference() {
-        return calculateCircumference();
-    }
-
-    public abstract double calculateArea(double radius);
-
-    public abstract double calculateArea(double height, double width);
-
-    public abstract double calculateCircumference(double radius);
-
-    public abstract double calculateCircumference(double height, double width);
 }
